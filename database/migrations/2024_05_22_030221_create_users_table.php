8822bf7c3f379b6cbee7621c6e2e69e848421712
Indra_Bagus_GolB_mobile <indrabusiness00@gmail.com>
2024-05-22 10:10:45 +0700

    migration

---
+++ database/migrations/2024_05_22_030221_create_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('email', 30)->unique();
+            $table->string('password', 30);
+            $table->string('no_hp', 15);
+            $table->string('saldo', 15);
+            $table->unsignedBigInteger('id_role');
+            $table->foreign('id_role')->references('id_role')->on('role');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
