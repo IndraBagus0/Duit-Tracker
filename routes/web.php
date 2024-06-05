4d51172f26c9e991bc4b449001c9d55df05bf0ed
DavidTimoteus <2131710012@student.polinema.ac.id>
2024-06-06 03:02:15 +0700

    Menambah Route-DataTransaksi

--- routes/web.php
+++ routes/web.php
@@ -46,12 +46,12 @@ Route::get('/redirect', [RedirectController::class, 'cek'])->name('redirect');
 
 Route::group(['middleware' => ['auth', 'checkrole:1']], function () {
     Route::get('/admin', [AdminController::class, 'index'])->name('admin');
-
     Route::get('/kategori', [KategoriController::class, 'index'])->name('daftarKategori');
     Route::get('/kategori/create', [KategoriController::class, 'create'])->name('createKategori');
     Route::post('/kategori/create', [KategoriController::class, 'store'])->name('storeKategori');
     Route::get('/kategori/{id_kategori}/edit', [KategoriController::class, 'edit'])->name('editKategori');
     Route::post('/kategori/{id_kategori}/edit', [KategoriController::class, 'update'])->name('updateKategori');
+    Route::get('/kategori/{id_kategori}/delete', [KategoriController::class, 'destroy'])->name('deleteKategori');
     Route::delete('/kategori/{id_kategori}/delete', [KategoriController::class, 'destroy'])->name('deleteKategori');
     Route::get('/users', [DataUserController::class, 'index'])->name('daftarUser');
     Route::get('/users/create', [DataUserController::class, 'create'])->name('createUser');
@@ -59,23 +59,22 @@ Route::group(['middleware' => ['auth', 'checkrole:1']], function () {
     Route::get('/users/{id}/edit', [DataUserController::class, 'edit'])->name('editUser');
     Route::post('/users/{id}/edit', [DataUserController::class, 'update'])->name('updateUser');
     Route::delete('/users/{id}/delete', [DataUserController::class, 'destroy'])->name('deleteUser');
-    Route::get('/kategori/{id_kategori}/delete', [KategoriController::class, 'destroy'])->name('deleteKategori');
     Route::get('/laporan', [LaporanController::class, 'index'])->name('laporan.index');
     Route::get('/laporan/print', [LaporanController::class, 'print'])->name('laporan.print');
-    Route::get('/transaksi/pendapatan', [TransaksiController::class, 'pendapatan'])->name('pendapatan');
-    Route::get('/transaksi/pengeluaran', [TransaksiController::class, 'pengeluaran'])->name('pengeluaran');
 });
 
 Route::group(['middleware' => ['auth', 'checkrole:2,3,4']], function () {
     Route::get('/user', [UserController::class, 'index'])->name('user');
-
     Route::get('/laporan', [LaporanController::class, 'index'])->name('laporan.index');
     Route::get('/laporan/print', [LaporanController::class, 'print'])->name('laporan.print');
-    Route::get('/transaksi/pendapatan', [TransaksiController::class, 'pendapatan'])->name('pendapatan');
-    Route::get('/transaksi/pengeluaran', [TransaksiController::class, 'pengeluaran'])->name('pengeluaran');
-
     Route::get('/profil', [ProfilUserController::class, 'index'])->name('profil');
     Route::post('/profil', [ProfilUserController::class, 'update'])->name('profil.update');
     Route::get('/profil/upgrade', [ProfilUserController::class, 'upgrade'])->name('profil.upgrade');
-
-}); 
+    Route::get('/transaksi/pendapatan', [TransaksiController::class, 'pendapatan'])->name('pendapatan');
+    Route::get('/transaksi/pendapatan/create', [TransaksiController::class, 'create'])->name('createIncome')->defaults('type', 'pendapatan');
+    Route::post('/transaksi/pendapatan', [TransaksiController::class, 'store'])->name('saveIncome');
+    Route::get('/transaksi/pengeluaran', [TransaksiController::class, 'pengeluaran'])->name('pengeluaran');
+    Route::post('/transaksi/pengeluaran', [TransaksiController::class, 'store'])->name('saveOutcome');
+    Route::get('/transaksi/pengeluaran/create', [TransaksiController::class, 'create'])->name('createOutcome')->defaults('type', 'pengeluaran');
+    Route::get('/transaksi/kategori', [KategoriController::class, 'index'])->name('idKategori');
+});
