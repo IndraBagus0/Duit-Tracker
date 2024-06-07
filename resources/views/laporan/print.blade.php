282d54cab58d8cc797b0829096e7f28131c3df29
DavidTimoteus <2131710012@student.polinema.ac.id>
2024-06-07 22:49:15 +0700

    perbaikan fungsi cetak data

--- resources/views/laporan/print.blade.php
+++ resources/views/laporan/print.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Cetak Laporan Transaksi</title>
     <style>
@@ -7,18 +8,24 @@
             width: 100%;
             border-collapse: collapse;
         }
-        table, th, td {
+
+        table,
+        th,
+        td {
             border: 1px solid black;
         }
-        th, td {
+
+        th,
+        td {
             padding: 8px;
             text-align: left;
         }
     </style>
 </head>
+
 <body>
     <h2>Laporan Transaksi</h2>
-    <p>Periode: {{ $startDate }} - {{ $endDate }}</p>
+    <p>Periode: {{ $startDate->format('j M Y') }} - {{ $endDate->format('j M Y') }}</p>
     <p>Jenis Transaksi: {{ $jenisTransaksi ? ucfirst($jenisTransaksi) : 'Semua' }}</p>
     <table>
         <thead>
@@ -32,25 +39,40 @@
             </tr>
         </thead>
         <tbody>
-        @foreach ($transaksi as $index => $trx)
-        <tr>
-            <td>{{ $index + 1 }}</td>
-            <td>{{ $trx->tanggal_transaksi }}</td>
-            <td>{{ $trx->kategori->nama_kategori }}</td>
-            <td>{{ number_format($trx->nominal_transaksi, 2) }}</td>
-            <td>{{ $trx->catatan_transaksi }}</td>
-            <td>{{ $trx->jenis_transaksi }}</td>
-        </tr>
-        @endforeach
+            @foreach ($transaksi as $index => $trx)
+                <tr>
+                    <td>{{ $index + 1 }}</td>
+                    <td>{{ $trx->tanggal_transaksi->format('j M Y') }}</td>
+                    <td>{{ $trx->kategori->nama_kategori }}</td>
+                    <td>{{ number_format($trx->nominal_transaksi, 2) }}</td>
+                    <td>{{ $trx->catatan_transaksi }}</td>
+                    <td>{{ $trx->jenis_transaksi }}</td>
+                </tr>
+            @endforeach
         </tbody>
         <tfoot>
             <tr>
-                <th colspan="3">Total Pemasukkan</th>
-                <th colspan="3">{{ number_format($totalPemasukan, 2) }}</th>
+                <th colspan="3">Saldo Awal</th>
+                <th colspan="3" style="text-align: end">{{ number_format($saldoAwal, 2) }}
+                </th>
             </tr>
             <tr>
-                <th colspan="3">Total Pengeluaran</th>
-                <th colspan="3">{{ number_format($totalPengeluaran, 2) }}</th>
+                <th colspan="3">Pemasukan</th>
+                <th colspan="3" style="text-align: end">
+                    {{ number_format($totalPemasukan, 2) }}</th>
+            </tr>
+            <tr>
+                <th colspan="3">Pengeluaran</th>
+                @php
+                    $formattedTotalPengeluaran = number_format($totalPengeluaran * -1, 2);
+                @endphp
+                <th colspan="3" style="text-align: end">{{ $formattedTotalPengeluaran }}
+                </th>
+            </tr>
+            <tr>
+                <th colspan="3">Saldo Akhir</th>
+                <th colspan="3" style="text-align: end">{{ number_format($totalSaldo, 2) }}
+                </th>
             </tr>
         </tfoot>
     </table>
@@ -58,4 +80,5 @@
         window.print();
     </script>
 </body>
+
 </html>
