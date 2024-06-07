d800a23c8563e5d5c5b5fd14fbf7906f40cd0d19
DavidTimoteus <2131710012@student.polinema.ac.id>
2024-06-07 22:48:46 +0700

    Perbaikan format & fungsi rentang tanggal

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -4,22 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Transaksi;
-use App\Models\Kategori;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Carbon;
 
 class LaporanController extends Controller
 {
     public function index(Request $request)
     {
-        $startDate = $request->input('start_date');
-        $endDate = $request->input('end_date');
+        $user = Auth::user();
+        $dateRange = $request->input('date_range');
         $jenisTransaksi = $request->input('jenis_transaksi');
-        $user_id = Auth::id();
 
-        $query = Transaksi::where('id_user', $user_id);
+        $startDate = null;
+        $endDate = null;
+        if ($dateRange) {
+            $dates = explode(' to ', $dateRange);
+            if (count($dates) == 2) {
+                $startDate = Carbon::parse($dates[0])->toDateString();
+                $endDate = Carbon::parse($dates[1])->toDateString();
+            } else {
+                return redirect()->back()->with('error', 'Harap Masukan Rentang Waktu');
+            }
 
-        if ($startDate && $endDate) {
-            $query->whereBetween('tanggal_transaksi', [$startDate, $endDate]);
+            $query = Transaksi::where('id_user', Auth::user()->id)
+                ->whereBetween('tanggal_transaksi', [$startDate, $endDate]);
+        } else {
+            $query = Transaksi::where('id_user', Auth::user()->id);
         }
 
         if ($jenisTransaksi) {
@@ -31,21 +41,24 @@ class LaporanController extends Controller
         // Menghitung total nominal berdasarkan jenis transaksi
         $totalPemasukan = $transaksi->where('jenis_transaksi', 'Pendapatan')->sum('nominal_transaksi');
         $totalPengeluaran = $transaksi->where('jenis_transaksi', 'Pengeluaran')->sum('nominal_transaksi');
+        $saldoAwal = $user->saldo;
+        $totalSaldo = $user->saldo + $totalPemasukan - $totalPengeluaran;
 
-        return view('laporan.index', compact('transaksi', 'startDate', 'endDate', 'totalPemasukan', 'totalPengeluaran','jenisTransaksi'));
+        return view('laporan.index', compact('transaksi', 'startDate', 'endDate', 'totalPemasukan', 'totalPengeluaran', 'jenisTransaksi', 'saldoAwal', 'totalSaldo'));
     }
+
 
     public function print(Request $request)
     {
-        $startDate = $request->input('start_date');
-        $endDate = $request->input('end_date');
+        $startDate = Carbon::parse($request->input('start_date'));
+        $endDate = Carbon::parse($request->input('end_date'));
         $jenisTransaksi = $request->input('jenis_transaksi');
-        $user_id = Auth::id();
+        $user = Auth::user();
 
-        $query = Transaksi::where('id_user', $user_id);
+        $query = Transaksi::where('id_user', $user->id);
 
         if ($startDate && $endDate) {
-            $query->whereBetween('tanggal_transaksi', [$startDate, $endDate]);
+            $query->whereBetween('tanggal_transaksi', [$startDate->startOfDay(), $endDate->endOfDay()]);
         }
 
         if ($jenisTransaksi) {
@@ -57,7 +70,9 @@ class LaporanController extends Controller
         // Menghitung total nominal berdasarkan jenis transaksi
         $totalPemasukan = $transaksi->where('jenis_transaksi', 'Pendapatan')->sum('nominal_transaksi');
         $totalPengeluaran = $transaksi->where('jenis_transaksi', 'Pengeluaran')->sum('nominal_transaksi');
+        $saldoAwal = $user->saldo;
+        $totalSaldo = $user->saldo + $totalPemasukan - $totalPengeluaran;
 
-        return view('laporan.print', compact('transaksi', 'startDate', 'endDate', 'totalPemasukan', 'totalPengeluaran','jenisTransaksi'));
+        return view('laporan.print', compact('transaksi', 'startDate', 'endDate', 'totalPemasukan', 'totalPengeluaran', 'jenisTransaksi', 'saldoAwal', 'totalSaldo'));
     }
 }
--- resources/views/laporan/index.blade.php
+++ resources/views/laporan/index.blade.php
@@ -1,91 +1,123 @@
 @extends('layouts.main')
 
 @section('contents')
+    <div id="app">
+        @include('layouts.sidebar')
+        <div id="main">
+            <header class="mb-3">
+                <a href="#" class="burger-btn d-block d-xl-none">
+                    <i class="bi bi-justify fs-3"></i>
+                </a>
+            </header>
 
-<div id="app">
-    @include('layouts.sidebar')
-    <div id="main">
-        <header class="mb-3">
-            <a href="#" class="burger-btn d-block d-xl-none">
-                <i class="bi bi-justify fs-3"></i>
-            </a>
-        </header>
-
-        <div class="page-heading">
-            <div class="page-title">
-                <div class="row align-items-center">
-                    <div class="col-12 col-md-6">
-                        <h3>Laporan Transaksi</h3>
-                        <p class="text-subtitle text-muted">Berikut adalah laporan transaksi berdasarkan kategori.</p>
-                    </div>
-                    <div class="col-12 col-md-6 mt-md-0">
-                        <div class="text-md-end mb-3 mb-md-0">
-                            <form method="GET" action="{{ route('laporan.index') }}" class="form-inline">
-                                <div class="input-group">
-                                    <input type="date" name="start_date" class="form-control me-2" value="{{ $startDate }}">
-                                    <input type="date" name="end_date" class="form-control me-2" value="{{ $endDate }}">
-                                    <select name="jenis_transaksi" class="form-control me-2">
-                                        <option value="">Jenis Transaksi</option>
-                                        <option value="Pendapatan" {{ $jenisTransaksi == 'Pendapatan' ? 'selected' : '' }}>Pendapatan</option>
-                                        <option value="Pengeluaran" {{ $jenisTransaksi == 'Pengeluaran' ? 'selected' : '' }}>Pengeluaran</option>
-                                    </select>
-                                    <button type="submit" class="btn btn-primary me-2">Filter</button>
-                                    <a href="{{ route('laporan.print', ['start_date' => $startDate, 'end_date' => $endDate, 'jenis_transaksi' => $jenisTransaksi]) }}" class="btn btn-secondary" target="_blank">Print</a>
-                                </div>
-                            </form>
+            <div class="page-heading">
+                <div class="page-title">
+                    <div class="row align-items-center">
+                        <div class="col-12 col-md-6">
+                            <h3>Laporan Transaksi</h3>
+                            <p class="text-subtitle text-muted">Berikut adalah laporan transaksi berdasarkan Jenis Transaksi
+                            </p>
                         </div>
-                    </div>
-                </div>
-            </div>
-
-            <section class="section">
-                <div class="row" id="table-bordered">
-                    <div class="col-12">
-                        <div class="card">
-                            <div class="card-content">
-                                <div class="table-responsive">
-                                    <table class="table table-bordered mb-0">
-                                        <thead>
-                                            <tr>
-                                                <th>NO</th>
-                                                <th>TANGGAL</th>
-                                                <th>KATEGORI</th>
-                                                <th>NOMINAL</th>
-                                                <th>CATATAN</th>
-                                                <th>JENIS</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                        @foreach ($transaksi as $index => $trx)
-                                        <tr>
-                                            <td>{{ $index + 1 }}</td>
-                                            <td>{{ $trx->tanggal_transaksi }}</td>
-                                            <td>{{ $trx->kategori->nama_kategori }}</td>
-                                            <td>{{ number_format($trx->nominal_transaksi, 2) }}</td>
-                                            <td>{{ $trx->catatan_transaksi }}</td>
-                                            <td>{{ $trx->jenis_transaksi }}</td>
-                                        </tr>
-                                        @endforeach
-                                        </tbody>
-                                        <tfoot>
-                                            <tr>
-                                                <th colspan="3">Total Pemasukan</th>
-                                                <th colspan="3">{{ number_format($totalPemasukan, 2) }}</th>
-                                            </tr>
-                                            <tr>
-                                                <th colspan="3">Total Pengeluaran</th>
-                                                <th colspan="3">{{ number_format($totalPengeluaran, 2) }}</th>
-                                            </tr>
-                                        </tfoot>
-                                    </table>
-                                </div>
+                        <div class="col-12 col-md-6 mt-md-0">
+                            <div class="text-md-end mb-3 mb-md-0">
+                                <form method="GET" action="{{ route('laporan.index') }}" class="form-inline">
+                                    <input type="date" name="date_range"
+                                        class="form-control flatpickr-range mb-3 flatpickr-input active"
+                                        placeholder="Pilih Rentang Tanggal...." readonly="readonly"
+                                        value="{{ request('date_range') }}" data-date-format="j M Y"
+                                        data-alt-format="j M Y">
+                                    <div class="input-group">
+                                        <select name="jenis_transaksi" class="form-control me-2">
+                                            <option value="">Jenis Transaksi</option>
+                                            <option value="Pendapatan"
+                                                {{ $jenisTransaksi == 'Pendapatan' ? 'selected' : '' }}>Pendapatan</option>
+                                            <option value="Pengeluaran"
+                                                {{ $jenisTransaksi == 'Pengeluaran' ? 'selected' : '' }}>Pengeluaran
+                                            </option>
+                                        </select>
+                                        <button type="submit" class="btn btn-primary me-2">Filter</button>
+                                        <a href="{{ route('laporan.print', ['start_date' => $startDate, 'end_date' => $endDate, 'jenis_transaksi' => $jenisTransaksi]) }}"
+                                            class="btn btn-secondary" target="_blank">Print</a>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
-            </section>
+                <br>
+                @if (session()->has('error'))
+                    <div class="alert alert-danger alert-dismissible show fade" role="alert">
+                        <i class="bi bi-exclamation-circle"></i> {{ session('error') }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                @endif
+                <br>
+                <section class="section">
+                    <div class="row" id="table-bordered">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-content">
+                                    <div class="table-responsive">
+                                        <table class="table table-bordered mb-0">
+                                            <thead>
+                                                <tr>
+                                                    <th>NO</th>
+                                                    <th>TANGGAL</th>
+                                                    <th>KATEGORI</th>
+                                                    <th>NOMINAL</th>
+                                                    <th>CATATAN</th>
+                                                    <th>JENIS</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                @foreach ($transaksi as $index => $trx)
+                                                    <tr>
+                                                        <td>{{ $index + 1 }}</td>
+                                                        <td>{{ $trx->tanggal_transaksi->format('j M Y') }}</td>
+                                                        <td>{{ $trx->kategori->nama_kategori }}</td>
+                                                        <td>{{ number_format($trx->nominal_transaksi, 2) }}</td>
+                                                        <td>{{ $trx->catatan_transaksi }}</td>
+                                                        <td>{{ $trx->jenis_transaksi }}</td>
+                                                    </tr>
+                                                @endforeach
+                                            </tbody>
+                                            <tfoot>
+                                                <tr>
+                                                    <th colspan="3">Saldo Awal</th>
+                                                    <th colspan="3" class="text-end">{{ number_format($saldoAwal, 2) }}
+                                                    </th>
+                                                </tr>
+                                                <tr>
+                                                    <th colspan="3">Pemasukan</th>
+                                                    <th colspan="3" class="text-end">
+                                                        {{ number_format($totalPemasukan, 2) }}</th>
+                                                </tr>
+                                                <tr>
+                                                    <th colspan="3">Pengeluaran</th>
+                                                    @php
+                                                        $formattedTotalPengeluaran = number_format(
+                                                            $totalPengeluaran * -1,
+                                                            2,
+                                                        );
+                                                    @endphp
+                                                    <th colspan="3" class="text-end">{{ $formattedTotalPengeluaran }}
+                                                    </th>
+                                                </tr>
+                                                <tr>
+                                                    <th colspan="3">Saldo Akhir</th>
+                                                    <th colspan="3" class="text-end">{{ number_format($totalSaldo, 2) }}
+                                                    </th>
+                                                </tr>
+                                            </tfoot>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            @include('layouts.footer')
         </div>
     </div>
-    @include('layouts.footer')
-</div>
 @endsection
